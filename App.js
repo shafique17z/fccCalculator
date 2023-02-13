0faf5d28555d630e9cd8ed7a15a8d91e99de44c6
Shafique Ahmed <shafiqued17z@gmail.com>
2023-02-13 18:22:23 +0500

    Fixed the state management using Hooks

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import Button from "./components/Button";
-import Row from "./components/Row";
-import calculator, { initialState } from "./util/calculator";
+import Button from "./app/components/Button";
+import Row from "./app/components/Row";
+import calculator, { initialState } from "./app/utils/calculator";
 
 export default function App() {
-  state = initialState;
+  const [state, setState] = useState(initialState);
 
   // handle tap method
-  HandleTap = (type, value) => {
-    this.setState((state) => calculator(type, value, state));
+  const HandleTap = (type, value) => {
+    setState((prevState) => calculator(type, value, prevState));
   };
 
   return (
     <View style={styles.container}>
-      {/* Status bae here */}
+      {/* Status bar settings here */}
       <SafeAreaView>
         <Text style={styles.value}>
-          {parseFloat(this.state.currentValue).toLocaleString()}
+          {parseFloat(state.currentValue).toLocaleString()}
         </Text>
 
         {/* Do create componentRow */}
@@ -25,69 +25,70 @@ export default function App() {
           <Button
             text="C"
             theme="secondary"
-            onPress={() => this.HandleTap("clear")}
+            onPress={() => HandleTap("clear")}
           />
 
           <Button
             text="+/-"
             theme="secondary"
-            onPress={() => this.HandleTap("posneg")}
+            onPress={() => HandleTap("posneg")}
           />
 
           <Button
             text="%"
             theme="secondary"
-            onPress={() => this.HandleTap("percentage")}
+            onPress={() => HandleTap("percentage")}
           />
 
           <Button
             text="/"
             theme="accent"
-            onPress={() => this.HandleTap("operator", "/")}
+            onPress={() => HandleTap("operator", "/")}
           />
         </Row>
 
         {/* Number */}
         <Row>
-          <Button text="7" onPress={() => this.HandleTap("number", 7)} />
-          <Button text="8" onPress={() => this.HandleTap("number", 8)} />
-          <Button text="9" onPress={() => this.HandleTap("number", 9)} />
+          <Button text="7" onPress={() => HandleTap("number", 7)} />
+          <Button text="8" onPress={() => HandleTap("number", 8)} />
+          <Button text="9" onPress={() => HandleTap("number", 9)} />
           <Button
             text="X"
             theme="accent"
-            onPress={() => this.HandleTap("operator", "*")}
+            onPress={() => HandleTap("operator", "*")}
           />
         </Row>
 
         <Row>
-          <Button text="5" onPress={() => this.HandleTap("number", 5)} />
-          <Button text="6" onPress={() => this.HandleTap("number", 6)} />
-          <Button text="7" onPress={() => this.HandleTap("number", 7)} />
+          <Button text="5" onPress={() => HandleTap("number", 5)} />
+          <Button text="6" onPress={() => HandleTap("number", 6)} />
+          <Button text="7" onPress={() => HandleTap("number", 7)} />
           <Button
             text="-"
             theme="accent"
-            onPress={() => this.HandleTap("operator", "-")}
+            onPress={() => HandleTap("operator", "-")}
           />
         </Row>
 
         <Row>
-          <Button text="1" onPress={() => this.HandleTap("number", 1)} />
-          <Button text="2" onPress={() => this.HandleTap("number", 2)} />
-          <Button text="3" onPress={() => this.HandleTap("number", 3)} />
+          <Button text="1" onPress={() => HandleTap("number", 1)} />
+          <Button text="2" onPress={() => HandleTap("number", 2)} />
+          <Button text="3" onPress={() => HandleTap("number", 3)} />
           <Button
             text="+"
             theme="accent"
-            onPress={() => this.HandleTap("operator", "+")}
+            onPress={() => HandleTap("operator", "+")}
           />
         </Row>
 
         <Row>
-          <Button text="0" onPress={() => this.HandleTap("number", 0)} />
-          <Button text="." onPress={() => this.HandleTap("number", ".")} />
+          <Button text="0" onPress={() => HandleTap("number", 0)}
+ />
+          <Button text="." onPress={() => HandleTap("number", ".")} />
           <Button
             text="="
             theme="primary"
-            onPress={() => this.HandleTap("equal", "=")}
+            onPress={() => HandleTap("equal", "=")}
           />
         </Row>
       </SafeAreaView>
